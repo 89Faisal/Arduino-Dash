71fbbce99ef052d829a6e15ec9cecbb3f0855581
89Faisal <f.rafiul@gmail.com>
2025-11-04 20:50:52 +0800

    added white LED and associated code

--- potLed.ino
+++ potLed.ino
@@ -1,11 +1,13 @@
 int potPin = A0;       // Potentiometer connected to A0
 int buttonPin = 7;     // Button connected to pin 7
 int ledPin = 8;        // LED connected to pin 8
+int ledPin2 = 4;       // White LED
 
 void setup() {
   Serial.begin(9600);  // Match this baud rate in Processing
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
@@ -17,6 +19,16 @@ void loop() {
   Serial.print(potValue);
   Serial.print(",");
   Serial.println(buttonValue);
+
+  // White LED Control from Button Switch
+  if (buttonValue == HIGH)
+  {
+    digitalWrite(ledPin2, HIGH);
+  }
+  else 
+  {
+    digitalWrite(ledPin2, LOW);
+  }
 
   // Check for serial commands from Processing
   if (Serial.available() > 0) {
